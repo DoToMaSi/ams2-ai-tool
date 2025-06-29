6a4531d732182e1ec36d55fb18acd1ef74c4607c
Douglas Tomacheski de Abreu e Silva <dotomasi@outlook.com>
2025-06-29 20:10:41 -0300

    - Project init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected title = 'ams2-ai-tool';
+  protected getCopyrightText = computed(() => {
+    const year = new Date().getFullYear();
+    return `©${
+      year !== 2025 ? '2025 - ' : ''
+    }${year} Developed by RockettSally - Automobilista 2, AMS2 and any other logo, names or references are property of Reiza Studios - All rights reserved. This is an unofficial tool and is not affiliated with Reiza Studios.`;
+  });
 }
